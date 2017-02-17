822012bad4280972a01702c661f45cfeb9598630
_meugen_ <eugen35@mail.ru>
2017-02-17 12:31:50 +0500

    На appWindow навесил простой детектор кручения колёсика с распознаванием клавиши и детектор движения мыши

--- components/AppWindow.js
+++ components/AppWindow.js
@@ -5,10 +5,28 @@ import NavToolBar from './NavToolBar'
 import CanvasView from './CanvasView'
 
 export default class AppWindow extends Component {
+  handleWheel = (e) => {
+    console.log('Направления', e.deltaY < 0 ? 'вверх' : 'вниз' );
+    console.log('Нажата ctrl ctrlKey:', e.ctrlKey ? 'да' : 'нет');
+    console.log('Нажата ctrl altKey:', e.altKey ? 'да' : 'нет');
+    console.log('Нажата ctrl shiftKey:', e.shiftKey ? 'да' : 'нет');
+  }
+
+  handleMouseMove = (e) => {
+    console.log('Координаты', e.clientX, ' ', e.clientY);
+    console.log('Нажата ctrl ctrlKey:', e.ctrlKey ? 'да' : 'нет');
+    console.log('Нажата ctrl altKey:', e.altKey ? 'да' : 'нет');
+    console.log('Нажата ctrl shiftKey:', e.shiftKey ? 'да' : 'нет');
+  }
+
+
+
+
   render() {
     //@todo [отдалённое] Имя класса убрать и убрать из index.html стиль appWindow1
     return (
-      <div className="appWindow" style={{width:'100%', height:'100%',borderColor: 'red', border:'solid'}}>
+      <div onMouseMove={this.handleMouseMove} onWheel={this.handleWheel} className="appWindow" style={{width:'100%', height:'100%',borderColor: 'red', border:'solid'}}>
+        <input type="text" id="one" onKeyPress={this.handleKeyPress}/>
         <NavToolBar/>
         <CanvasView/>
       </div>
